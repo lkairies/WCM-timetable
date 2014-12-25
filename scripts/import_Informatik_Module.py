8d7555b2717859bb9416d57cbf2bc17d1acb41b2
Lukas Fischer <joki0z@web.de>
2014-12-25 16:49:26 +0100

    extract finite state machines from parsers
    bachelor parser fails to parse Modulnamen.
    both parsers do not parse Modulnummern.
    parsers seem to work ok for Leistungspunkte.
    master parser seems to be in better shape than bachelor parser.

--- scripts/import_Informatik_Module.py
+++ scripts/import_Informatik_Module.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
+#################################
+# vorraussetzungen: python3, pdfminer, python3-urllib3
+#
+import parse_bachelor, parse_master
+import urllib.request
+import os
 
 current_modules_bachelor = \
-  'http://www.informatik.uni-leipzig.de/ifi/studium/studiengnge/ba-inf/ba-inf-module.html'
+'http://www.informatik.uni-leipzig.de/ifi/studium/studiengnge/ba-inf/ba-inf-module.html'
 current_modules_master = \
-  'http://www.informatik.uni-leipzig.de/ifi/studium/studiengnge/ma-inf/ma-inf-module.html'
+'http://www.informatik.uni-leipzig.de/ifi/studium/studiengnge/ma-inf/ma-inf-module.html'
+
+testurl_bachelor = 'file:///home/joki/Studium/Master/WCM/WCM-timetable/scripts/test/ba-inf-module.pdf'
+testurl_master = 'file:///home/joki/Studium/Master/WCM/WCM-timetable/scripts/test/ma-inf-module.pdf'
+
+#TODO: use streams instead of files and put this into a function.
+#~ r = urllib.request.urlopen(testurl_bachelor)
+#~ with open('/tmp/ba-inf.pdf', 'wb') as f:
+    #~ f.write(r.read())
+#~ os.system('pdf2txt -o /tmp/a.out /tmp/ba-inf.pdf');
+#~ bobj = open("/tmp/a.out", "r")
 
 
+bobj = open("/home/joki/Studium/Master/WCM/WCM-timetable/scripts/test/ba-inf-module.txt", "r")
+parse_bachelor.parse(bobj)
+
+
+mobj = open("/home/joki/Studium/Master/WCM/WCM-timetable/scripts/test/ma-inf-module.txt", "r")
+parse_master.parse(mobj)
--- scripts/parse_bachelor.py
+++ scripts/parse_bachelor.py
@@ -1,125 +1,119 @@
-#!/usr/bin/env python
-from urllib2 import urlopen
-from timetable.models import Department, Event, EventType, FieldOfStudy, Instructor, Location, Modul, ModulFieldOfStudy, Semester, ModulType
+#!/usr/bin/env python3
 import os, sys, re
 
-class BAInfParser():
-    def fetch(self, url):
-        department, created = Department.objects.get_or_create(name='Institut für Informatik')
+def parse(pdf2txt_stream):
+    fobj = pdf2txt_stream
+    department = 'Institut für Informatik'
 
-        r = urlopen(url)
-        with open('/tmp/ba-inf.pdf', 'w') as f:
-            f.write(r.read())
-        os.system('pdf2txt.py -o /tmp/a.out /tmp/ba-inf.pdf');
+    counter=0
+    counter2 =0
+    founddate=0
 
-        fobj = open("/tmp/a.out", "r")
+    index=1
+    flag=0
+    flagInhalt=0
+    flagInhaltException=0
 
-        counter=0
-        counter2 =0
-        founddate=0
+    liste = []
+    liste2 = []
+    titles =[]
+    numbers=[]
 
-        index=1
-        flag=0
-        flagInhalt=0
-        flagInhaltException=0
+    letzteZeile='leer'
 
-        liste = []
-        liste2 = []
-        titles =[]
-        numbers=[]
+    title=''
+    lp='0'
+    inhalt=''
+    modultype=''
 
-        letzteZeile='leer'
+    for line in fobj:
+        liste.insert(counter, line)
+        counter+=1
 
-        title=''
-        lp='0'
-        inhalt=''
-        modultype=''
+        #modulnummer
+        if counter==1:
+            number=line
 
-        for line in fobj:
-            liste.insert(counter, line)
-            counter+=1
+        #Leistungspunkte
+        if '5 LP = 150 Arbeitsstunden (Workload)' in line:index+=1; lp=5
+        if '10 LP = 300 Arbeitsstunden (Workload)' in line:index+=1; lp=10
 
-            #modulnummer
-            if counter==1:
-                number=line
+        #modultitel bachelor
 
-            #Leistungspunkte
-            if '5 LP = 150 Arbeitsstunden (Workload)' in line:index+=1; lp=5
-            if '10 LP = 300 Arbeitsstunden (Workload)' in line:index+=1; lp=10
+        #inhalt
+        if line.strip() == 'Teilnahmevoraus-':
+            flagInhalt=0
+            if inhalt.strip()=='':
+                #print 'fehler'+number
+                flagInhaltException=1
+                 #inhalt steht dann zwischen 'Literaturangabe' und 'keine'
 
-            #modultitel bachelor
-
-            #inhalt
-            if line.strip() == 'Teilnahmevoraus-':
-                flagInhalt=0
-                if inhalt.strip()=='':
-                    #print 'fehler'+number
-                    flagInhaltException=1
-                     #inhalt steht dann zwischen 'Literaturangabe' und 'keine'
-
-            if flagInhalt==1: inhalt += line
-            if line.strip() =='Inhalt': flagInhalt=1
+        if flagInhalt==1: inhalt += line
+        if line.strip() =='Inhalt': flagInhalt=1
 
 
-            #inhalt Ausnahmebehandlung
-            if line.strip() == 'keine': flagInhaltException=0
-            if flagInhaltException==2: inhalt += line
-            if line.strip() =='Literaturangabe' and flagInhaltException==1: flagInhaltException=2
+        #inhalt Ausnahmebehandlung
+        if line.strip() == 'keine': flagInhaltException=0
+        if flagInhaltException==2: inhalt += line
+        if line.strip() =='Literaturangabe' and flagInhaltException==1: flagInhaltException=2
 
-            letzteZeile=line
+        letzteZeile=line
 
-            #str = line
-            match = re.search(r'\d\d?\.\s\w+\s\d{4}', line)
-            # If-statement after search() tests if it succeeded
-            #if match:
-                #print 'found', match.group() ## 'found word:cat'
-                #founddate+=1
-                #print founddate
+        #str = line
+        match = re.search(r'\d\d?\.\s\w+\s\d{4}', line)
+        # If-statement after search() tests if it succeeded
+        #if match:
+            #print 'found', match.group() ## 'found word:cat'
+            #founddate+=1
+            #print founddate
 
-            if 'Vertiefungsmodul'==line.strip(): modultype='Vertiefungsmodul'
-            if 'Ergänzungsfach'==line.strip(): modultype='Ergänzungsfach'
-            if 'Kernmodul'==line.strip(): modultype='Kernmodul'
-            if 'Seminarmodul'==line.strip(): modultype='Seminarmodul'
-            if 'Ergänzungsfach Medizinische Informatik'==line.strip(): modultype='Ergänzungsfach Medizinische Informatik'
+        if 'Vertiefungsmodul'==line.strip(): modultype='Vertiefungsmodul'
+        if 'Ergänzungsfach'==line.strip(): modultype='Ergänzungsfach'
+        if 'Kernmodul'==line.strip(): modultype='Kernmodul'
+        if 'Seminarmodul'==line.strip(): modultype='Seminarmodul'
+        if 'Ergänzungsfach Medizinische Informatik'==line.strip(): modultype='Ergänzungsfach Medizinische Informatik'
 
 
-            #neuer eintrag
-            if 'Modulnummer' in line:
-                flag+=1
+        #neuer eintrag
+        if 'Modulnummer' in line:
+            flag+=1
 
-                if title.strip()=='Wahrscheinlichkeitstheorie':
-                    inhalt='Einführung in die Denkweisen und Beweismethoden der W\'theorie, Erschließung wichtiger Einsatz- und Anwendungsgebiete der Mathematik diskrete Wahrscheinlichkeitsräume und Wahrscheinlichkeiten mit Dichten: grundlegende Konzepte (Erwartungswert, Varianz, Unabhängigkeit, Zufallsgrößen), Beispiele für Verteilungen, Gesetz der Großen Zahlen, Satz von Moivre-Laplace, einführende Betrachtungen der mathematischen Statistik (Schätztheorie, Konfidenzbereiche, Testtheorie)'
+            if title.strip()=='Wahrscheinlichkeitstheorie':
+                inhalt='Einführung in die Denkweisen und Beweismethoden der W\'theorie, Erschließung wichtiger Einsatz- und Anwendungsgebiete der Mathematik diskrete Wahrscheinlichkeitsräume und Wahrscheinlichkeiten mit Dichten: grundlegende Konzepte (Erwartungswert, Varianz, Unabhängigkeit, Zufallsgrößen), Beispiele für Verteilungen, Gesetz der Großen Zahlen, Satz von Moivre-Laplace, einführende Betrachtungen der mathematischen Statistik (Schätztheorie, Konfidenzbereiche, Testtheorie)'
 
-                counter=0
-                liste2=liste
-                liste[:]=[]
-                #print title
-                if counter2>0:
-                    titles.append(title.strip() )
-                    numbers.append(number.strip())
-                    #print titles[counter2-1]+numbers[counter2-1]
-                counter2+=1
+            counter=0
+            liste2=liste
+            liste[:]=[]
+            #print title
+            if counter2>0:
+                titles.append(title.strip() )
+                numbers.append(number.strip())
+                #print titles[counter2-1]+numbers[counter2-1]
+            counter2+=1
 
-                type, created = ModulType.objects.get_or_create(name=modultype)
-                modul, created = Modul.objects.get_or_create(number=number.strip())
-                if created or not modul.name:
-                    modul.name = title.strip()
-                modul.lp=lp
-                modul.modultype=type
-                modul.description=inhalt
-                modul.department = department
-                #modul.fields='no clue what that is'
-                modul.save()
+            #~ type, created = ModulType.objects.get_or_create(name=modultype)
+            #~ modul, created = Modul.objects.get_or_create(number=number.strip())
+            #~ if created or not modul.name:
+                #~ modul.name = title.strip()
+            #~ modul.lp=lp
+            #~ modul.modultype=type
+            #~ modul.description=inhalt
+            #~ modul.department = department
+            #~ modul.save()
+            print("Abteilung: ", department)
+            print("Name: ", title.strip())
+            print("LP: ", lp)
+            print("Modultyp: ", modultype)
+            print("Beschreibung: ", len(inhalt))
 
 
 
 
 
+            title=''
+            number=''
+            lp='0'
+            inhalt=''
+            modultype=''
 
-                title=''
-                number=''
-                lp='0'
-                inhalt=''
-                modultype=''
-
-        fobj.close()
+    fobj.close()
