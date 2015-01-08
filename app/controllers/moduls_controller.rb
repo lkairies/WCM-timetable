869f3dece48594947e9e983fc75117d6cf63667c
Lukas Fischer <joki0z@web.de>
2015-01-08 19:55:12 +0100

    improve modul index and modul show
    modul index will now ignore Moduls which don't have any LVs linked to them.
    lvs with the same titel will now be displayed only once per modul.
    detailed modul view now groups by lvs with the same titel.

--- app/controllers/moduls_controller.rb
+++ app/controllers/moduls_controller.rb
@@ -1,9 +1,28 @@
 class ModulsController < ApplicationController
   def show
     @modul = Modul.where(nummer: params[:id]).first
+    #does Hash.new( Array.new ) work? New array as default Hash value.
+    @lvs = Hash.new
+    Lehrveranstaltung.where(modul_id: @modul.nummer).each do |lv|
+      unless @lvs.has_key?(lv.titel)
+        lvlist = Array.new
+        @lvs[lv.titel]=lvlist
+      end
+      @lvs[lv.titel].push(lv)
+    end
   end
 
   def index
     @moduls = Modul.all
+    @modul_lvs = Hash.new
+    Lehrveranstaltung.all.each do |lv|
+      unless @modul_lvs.has_key?(lv.modul_id)
+        lvlist = Array.new
+        @modul_lvs[lv.modul_id]=lvlist
+      end
+      unless @modul_lvs[lv.modul_id].include?(lv.titel)
+        @modul_lvs[lv.modul_id].push(lv.titel)
+      end
+    end
   end
 end
