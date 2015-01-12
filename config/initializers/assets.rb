8b6c32bf09e9771637145ebc139be581c127769a
Lukas Kairies <lkairies90@gmail.com>
2015-01-12 17:45:13 +0100

    Fixed includes for Moduls#index view

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,5 +7,10 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( bootstrap-table-filter.css )
+Rails.application.config.assets.precompile += %w( bootstrap-table.min.css )
+
 Rails.application.config.assets.precompile += %w( bootstrap-table-filter.js )
-Rails.application.config.assets.precompile += %w( ext/bs-table.js )
+Rails.application.config.assets.precompile += %w( bs-table.js )
+Rails.application.config.assets.precompile += %w( bootstrap-table.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap-table-filter.min.js )
+
