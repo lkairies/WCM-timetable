ecf71837953742185b685bc1a199c48ba140f364
Lukas Fischer <joki0z@web.de>
2015-01-24 00:53:49 +0100

    seed semester information from website uni-leipzig.de

--- app/controllers/wochenplan_controller.rb
+++ app/controllers/wochenplan_controller.rb
@@ -2,43 +2,13 @@ require 'icalendar'
 require 'date'
 require 'tzinfo'
 class WochenplanController < ApplicationController
-  def get_vorlesungstage(semester_begin, semester_end)
-
-    vorlesungstage = Array(0..(semester_end-semester_begin))
-    #logger.debug "vorlesungstage (roh): #{vorlesungstage}"
-
-
-    #TODO: filter events by free days: https://www.zv.uni-leipzig.de/studium/studienorganisation/akademisches-jahr.html
-    vorlesungsfrei = ["31.10.2014 (Freitag) vorlesungsfrei", "19.11.2014 (Mittwoch) vorlesungsfrei", "vom 21.12.2014 bis 04.01.2015 vorlesungsfrei"]
-
-    vorlesungsfrei.each do |str|
-      if str.include? "vorlesungsfrei"
-        #logger.debug "break: #{str}"
-        if str.include? " bis "
-          dates = str.split(" bis ")
-          break_begin = Date.parse(dates[0])
-          break_end = Date.parse(dates[1])+1 # add one because the end of break date is included in the break
-          #logger.debug "from #{break_begin}   to #{break_end}   -   length: #{break_end-break_begin}"
-          vorlesungstage -= Array((break_begin-semester_begin).to_i..(break_end-semester_begin).to_i)
-        else
-          break_date = Date.parse(str)
-          #logger.debug "at: #{break_date}"
-          vorlesungstage -= [(break_date-semester_begin).to_i]
-        end
-      end
-    end
-    #logger.debug "vorlesungstage (reduziert): #{vorlesungstage}"
-    return vorlesungstage
-
-  end
 
   def get_lv_events(lv)
     events = Array.new
     unless lv[:zeit_von]
       return events
     end
-    semester_begin = Date.parse("2014-10-13")
-    semester_end = Date.parse("2015-02-07")
+    semester = Semester.find_by(semester_id: lv[:semester])
 
     #TODO: decode pattern:
     # 11./12.01.2014 (Blockveranstaltung)
@@ -92,9 +62,9 @@ class WochenplanController < ApplicationController
     #TODO: use a configurable parameter for this:
     lv_time_zone = TZInfo::Timezone.get('Europe/Berlin')
 
-    get_vorlesungstage(semester_begin, semester_end).each do |day|
-      if doubleweek[(day+semester_begin.wday)%14]
-        date = semester_begin + day
+    semester[:vorlesungstage].each do |day|
+      if doubleweek[(day+semester.begin.wday)%14]
+        date = semester.begin + day
         event = Icalendar::Event.new
 
         # the naming of time functions is very confusing, please read the documentation of tzinfo
