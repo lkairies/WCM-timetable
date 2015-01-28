8be5edbfbace59850f1d3b368e1beeff81b6450a
Lukas Fischer <joki0z@web.de>
2015-01-28 21:05:33 +0100

    for empty semester parameter fall back to semester detection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   protected def selected_semester
-    if params[:semester]
+    if params[:semester] and params[:semester] != ''
       return params[:semester]
     else
       require 'date'
