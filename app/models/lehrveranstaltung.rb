0b5a039a70c24877d63809c35d7b3d9e915c6e8d
Lukas Fischer <joki0z@web.de>
2015-08-08 12:24:53 +0200

    update model Lehrveranstaltung to reflect changes in odfmi

--- app/models/lehrveranstaltung.rb
+++ app/models/lehrveranstaltung.rb
@@ -9,5 +9,5 @@ class Lehrveranstaltung < ActiveRecord::Base
    serialize :dozent
 
    # list taken from http://pcai003.informatik.uni-leipzig.de:8892/sparql?default-graph-uri=&query=++SELECT+DISTINCT+%3Ftype%0D%0A++++WHERE%0D%0A++++%7B%0D%0A++++++%3Ftype+rdfs%3AsubClassOf+od%3ALV%0D%0A++++%7D&format=text%2Fhtml&timeout=0&debug=on
-   enum form: [:Oberseminar, :Onlinekurs, :Praktikum, :Seminar, :Uebung, :Vorlesung, :VLUeb]
+   enum form: [:Project, :Oberseminar, :Onlinekurs, :Praktikum, :Seminar, :Uebung, :Vorlesung, :VLUeb]
 end
